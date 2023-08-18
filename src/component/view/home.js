9420acb4c48a8044f0b4628268a5e457f9a56eac
kiran-lns <kiran@lockandstock.app>
2023-08-18 13:52:35 -0400

    minor change

--- src/component/view/home.js
+++ src/component/view/home.js
@@ -48,7 +48,7 @@ class Home extends React.Component {
           }}
         >
           <h4 style={{ marginLeft: "20px", textAlign: "center" }}>
-            Recently added
+            Recently added products
           </h4>
         </div>
         <div className="row">
